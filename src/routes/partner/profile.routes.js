8f0b6555e8510d614f3d02946fdc1921a77b6042
nguyentruongthinh12c8tv-bin <nguyentruongthinh12c8.tv@gmail.com>
2026-06-20 19:12:50 +0700

    feat(partner_profile_management): add feature View Partner Profile

--- src/routes/partner/profile.routes.js
+++ src/routes/partner/profile.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const partnerProfileController = require('../../controllers/partner/partnerProfile.controller');
 const authenticate = require('../../middlewares/auth.middleware');
-
+const { restrictTo } = require('../../middlewares/role.middleware');
+const { PARTNER } = require('../../constants/roles');
 const router = express.Router();
 
 // Get current partner's profile (protected route)
-router.get('/me', authenticate, partnerProfileController.getProfile);
+router.get('/me', authenticate, restrictTo(PARTNER), partnerProfileController.getProfile);
 
 module.exports = router;
